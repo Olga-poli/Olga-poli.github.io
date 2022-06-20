a6b3d84ccc6a874706e76c4ffb287238f30929a7
Olha Polishko <polishko.official@gmail.com>
2022-06-20 17:55:10 +0300

    HW1: MovieListItem wip

--- src/components/MovieListItem/MovieListItem.jsx
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './MovieListItem.scss';
 
-function MovieListItem() {
-  return (
-    <div className="card movies-list-item">
-      <div className="card-body">
-        <div className="buttons-group">
-          <button type="button" className="btn btn-light btn-sm">
-            <i className="fa fa-thumbs-up" />
-          </button>
-          <button type="button" className="btn btn-light btn-sm">
-            <i className="fa fa-thumbs-down" />
-          </button>
+// eslint-disable-next-line react/prefer-stateless-function
+class MovieListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // eslint-disable-next-line react/prop-types
+      title: props.data.title,
+    };
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <div className="card movies-list-item">
+        <div className="card-body">
+          <div className="buttons-group">
+            <button type="button" className="btn btn-light btn-sm">
+              <i className="fa fa-thumbs-up" />
+            </button>
+            <button type="button" className="btn btn-light btn-sm">
+              <i className="fa fa-thumbs-down" />
+            </button>
+          </div>
+          <span>likes</span>
+          <span>1</span>
+          <h3 className="card-title">{title}</h3>
         </div>
-        <span>likes</span>
-        <span>1</span>
-        <h3 className="card-title">Titanic</h3>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MovieListItem;
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
 import MovieListItem from '../MovieListItem';
 
-function MoviesList() {
+function MoviesList({ movies }) {
+  const moviesItems = movies
+    ? movies.map((item) => (<MovieListItem data={item} key={item.id} />))
+    : null;
   return (
     <div className="movies-list">
-      <MovieListItem />
-      <MovieListItem />
-      <MovieListItem />
-      <MovieListItem />
-      <MovieListItem />
-      <MovieListItem />
+      {moviesItems}
     </div>
   );
 }
+
+MoviesList.defaultProps = {
+  movies: PropTypes.shape({}),
+};
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    adult: PropTypes.bool,
+    backdrop_path: PropTypes.string,
+    genre_ids: PropTypes.arrayOf(PropTypes.number),
+    id: PropTypes.number,
+    original_language: PropTypes.string,
+    original_title: PropTypes.string,
+    overview: PropTypes.string,
+    popularity: PropTypes.number,
+    poster_path: PropTypes.string,
+    release_date: PropTypes.string,
+    title: PropTypes.string,
+    video: PropTypes.bool,
+    vote_average: PropTypes.number,
+    vote_count: PropTypes.number,
+  })),
+};
 
 export default MoviesList;
