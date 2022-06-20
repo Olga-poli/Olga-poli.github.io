cb9f566c8be22a66b952b44a74710143141b88f6
Olha Polishko <polishko.official@gmail.com>
2022-06-20 16:12:19 +0300

    HW1: filter buttons fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      filter: 'rating', // likes, rating
+      filter: '',
     };
   }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const app = new App();
-root.render(app.render());
+root.render(<App />);
