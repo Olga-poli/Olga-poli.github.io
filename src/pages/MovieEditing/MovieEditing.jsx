71d38e14d5dece4a3f3356fbaa7c3faa62d17aa0
Olha Polishko <polishko.official@gmail.com>
2022-07-07 10:59:08 +0300

    HW5: formik added

--- src/pages/MovieEditing/MovieEditing.jsx
+++ src/pages/MovieEditing/MovieEditing.jsx
@@ -1,8 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
 import { useHistory, useParams } from 'react-router-dom';
 import { updateMovieItem } from '../../store/slices/catalog.slice';
 import styles from './MovieEditing.module.scss';
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.movieTitle) {
+    errors.movieTitle = 'Required';
+  } else if (!/^[A-Z][a-zA-Z0-9 -—.,!?:]+/.test(values.movieTitle)) {
+    errors.movieTitle = 'Must have title case and two symbols at least';
+  }
+
+  if (!values.moviePosterPath) {
+    errors.moviePosterPath = 'Required';
+  } else if (!/^\/[a-zA-Z0-9]*(.jpg|.jpeg|.png|.gif)/.test(values.moviePosterPath)) {
+    errors.moviePosterPath = 'Must have right file extension and start with "/"';
+  }
+
+  if (!values.movieOverview) {
+    errors.movieOverview = 'Required';
+  } else if (!/^[A-Z][a-zA-Z0-9 -—.,!?:]+/.test(values.movieOverview)) {
+    errors.movieOverview = 'Overview is not long enough';
+  }
+
+  return errors;
+};
 
 function MovieEditing() {
   const dispatch = useDispatch();
@@ -12,112 +36,75 @@ function MovieEditing() {
   const moviesItemsList = useSelector((state) => state.catalogReducer.moviesItemsList);
   const currentMovieData = moviesItemsList.find(({ id }) => id === Number(movieID));
 
-  const [movieTitleState, setMovieTitleState] = useState('');
-  const [moviePosterPathState, setMoviePosterPathState] = useState('');
-  const [movieOverviewState, setOverviewState] = useState('');
-  const [errorMessages, setErrorMessages] = useState({
-    movieTitleState: '',
-    movieDirectorState: '',
-    moviePosterPathState: '',
-    movieOverviewState: '',
-  });
-
   const {
     title,
     poster_path: posterPath,
     overview,
   } = currentMovieData;
 
-  useEffect(() => {
-    setMovieTitleState(title);
-    setMoviePosterPathState(posterPath);
-    setOverviewState(overview);
-  }, []);
-
-  if (!currentMovieData) {
-    return null;
-  }
-
-  const onSubmit = ((event) => {
-    event.preventDefault();
-    const newMovieData = {
-      title: movieTitleState,
-      poster_path: moviePosterPathState,
-      overview: movieOverviewState,
-    };
-    dispatch(updateMovieItem({ id: Number(movieID), newMovieData }));
-    history.push(`/catalog/${movieID}`);
+  const formik = useFormik({
+    initialValues: {
+      movieTitle: title,
+      moviePosterPath: posterPath,
+      movieOverview: overview,
+    },
+    validate,
+    onSubmit: (values) => {
+      const newMovieData = {
+        title: values.movieTitle,
+        poster_path: values.moviePosterPath,
+        overview: values.movieOverview,
+      };
+      dispatch(updateMovieItem({ id: Number(movieID), newMovieData }));
+      history.push(`/catalog/${movieID}`);
+    },
   });
-
-  const handleUserInput = (event) => {
-    const { name, value } = event.target;
-    let errorMessage = '';
-    const map = {
-      movieTitleState: () => {
-        setMovieTitleState(value);
-        errorMessage = /^[A-Z][a-zA-Z0-9 -—.,!?:]+/.test(value) ? '' : 'Incorrect title';
-        setErrorMessages({ ...errorMessages, movieTitleState: errorMessage });
-      },
-      moviePosterPathState: () => {
-        setMoviePosterPathState(value);
-        errorMessage = /^\/[a-zA-Z0-9]*(.jpg|.png|.gif)/.test(value) ? '' : 'Incorrect path';
-        setErrorMessages({ ...errorMessages, moviePosterPathState: errorMessage });
-      },
-      movieOverviewState: () => {
-        setOverviewState(value);
-        errorMessage = /^[A-Z][a-zA-Z0-9 -—.,!?:]+/.test(value) ? '' : 'Overview is not long enough';
-        setErrorMessages({ ...errorMessages, movieOverviewState: errorMessage });
-      },
-    };
-
-    map[name](value);
-  };
 
   return (
     <div className={styles.movieEditing}>
       <button onClick={() => history.goBack()} type="button" className={`${styles.button} btn btn-secondary`}>Go back</button>
       <h2>Edit</h2>
       <form
-        onSubmit={onSubmit}
+        onSubmit={formik.handleSubmit}
         className={styles.form}
       >
         <div className={styles.inputBlock}>
           <label htmlFor="title" className="form-label">Title</label>
           <input
-            onChange={(event) => handleUserInput(event)}
-            defaultValue={title}
-            name="movieTitleState"
+            onChange={formik.handleChange}
+            value={formik.values.movieTitle}
+            name="movieTitle"
             className="form-control"
             type="text"
             id="title"
             required
           />
-          <div className={styles.formText}>{errorMessages.movieTitleState}</div>
+          {formik.errors.movieTitle ? <div>{formik.errors.movieTitle}</div> : null}
         </div>
         <div className={styles.inputBlock}>
           <label htmlFor="image">Image url</label>
           <input
-            onChange={(event) => handleUserInput(event)}
-            defaultValue={posterPath}
-            name="moviePosterPathState"
+            onChange={formik.handleChange}
+            value={formik.values.moviePosterPath}
+            name="moviePosterPath"
             className="form-control"
             type="text"
             id="image"
             required
           />
-          <div className={styles.formText}>{errorMessages.moviePosterPathState}</div>
+          {formik.errors.moviePosterPath ? <div>{formik.errors.moviePosterPath}</div> : null}
         </div>
         <div className={styles.inputBlock}>
           <label htmlFor="description">Description</label>
           <textarea
-            onChange={(event) => handleUserInput(event)}
-            defaultValue={overview}
-            name="movieOverviewState"
+            onChange={formik.handleChange}
+            value={formik.values.movieOverview}
+            name="movieOverview"
             className="form-control"
             id="description"
             required
           />
-          <div className={styles.formText}>{errorMessages.movieOverviewState}</div>
+          {formik.errors.movieOverview ? <div>{formik.errors.movieOverview}</div> : null}
         </div>
         <button className={`${styles.button} btn btn-primary`} type="submit">Submit form</button>
       </form>
